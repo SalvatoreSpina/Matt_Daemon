901346c8d25940e85a027a4ce308f45ff05ac9c1
Salvatore Spina <saske0109@gmail.com>
2023-02-02 09:53:43 +0100

    feat: Makefile & main added

---
+++ Makefile
@@ -0,0 +1,21 @@
+NAME =		matt_daemon
+
+COMP =		clang++
+CFLAGS =	-Wall -Werror -Wextra
+
+SRCS =		srcs/main.cpp
+
+OBJS =		$(SRCS:%.cpp=%.o)
+
+$(NAME):	$(SRCS)
+	$(COMP) $(CFLAGS) $(SRCS) -o $(NAME)
+
+all: $(NAME)
+
+clean:
+			$(RM) $(OBJS)
+
+fclean:		clean
+			$(RM) $(NAME)
+
+re:			fclean $(NAME)
---
+++ srcs/main.cpp
@@ -0,0 +1,71 @@
+#include "../includes/Daemon.hpp"
+#include "../includes/Tintin_reporter.hpp"
+
+// Function to handle signals
+void HandleSignal(int signum) {
+	(void)signum;
+	Tintin_reporter::WriteLogs(Tintin_reporter::kInfo, "Signal handler.");
+	throw Daemon::QuitRequested();
+}
+
+// Creates log/lock directories and files
+void CreateEnvironment(int& lock_file_descriptor)
+{
+	int ret;
+
+	if (getuid() != 0) {
+		throw Daemon::FilePermissionError();
+	}
+	ret = mkdir(LOG_DIRECTORY, 0777);
+	if (ret == -1 && errno != EEXIST) {
+		perror(LOG_DIRECTORY);
+		throw Daemon::FunctionCallError();
+	}
+	ret = mkdir(LOCK_DIRECTORY, 0777);
+	if (ret == -1 && errno != EEXIST) {
+		perror(LOCK_DIRECTORY);
+		throw Daemon::FunctionCallError();
+	}
+	lock_file_descriptor = open(LOCK_FILE, O_CREAT, 0644);
+	if (lock_file_descriptor == -1) {
+		perror(LOCK_FILE);
+		throw Daemon::FunctionCallError();
+	}
+}
+
+int main()
+{
+	int pid, lock_file_descriptor;
+	int ret = kOk;
+
+	// Register signals to handle
+	signal(SIGINT, HandleSignal);
+	signal(SIGQUIT, HandleSignal);
+	signal(SIGTERM, HandleSignal);
+
+	try {
+		CreateEnvironment(lock_file_descriptor);
+		pid = fork();
+		if (pid == 0) {
+			Daemon daemon(lock_file_descriptor);
+			daemon.LockFile();
+			daemon.InitializeServer();
+			daemon.Start();
+		} else if (pid < 0) {
+			perror("Daemon: fork");
+			throw Daemon::FunctionCallError();
+		}
+	} catch(const Daemon::FileLockError& e) {
+		std::cerr << e.what() << std::endl;
+	} catch(const Daemon::QuitRequested& e) {
+		// Do nothing
+	} catch(const Daemon::FilePermissionError& e) {
+		std::cerr << e.what() << std::endl;
+		ret = kFailed;
+	} catch(const Daemon::FunctionCallError& e) {
+		ret = kFailed;
+	} catch(const std::exception& e) {
+		ret = kFailed;
+	}
+	return ret;
+}
